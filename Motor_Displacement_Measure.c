66a624670dd75dd5412199ff4b42b871cd428519
jared-gal <43042105+jared-gal@users.noreply.github.com>
2018-11-19 23:51:36 -0500

    Update Motor_Displacement_Measure.c

--- Motor_Displacement_Measure.c
+++ Motor_Displacement_Measure.c
@@ -100,8 +100,9 @@ void __ISR(_TIMER_3_VECTOR, ipl2) Timer3Handler(void)
                   mPORTBClearBits(stp3.NDIR);
                   mPORTBClearBits(stp3.STP);
                   ct = 0;
+                  stp3.stpLeft--;
               }
-
+        }
         //clear the interrupt flag
         mT3ClearIntFlag();
 }
@@ -113,11 +114,26 @@ void __ISR(_TIMER_3_VECTOR, ipl2) Timer3Handler(void)
 static PT_THREAD (protothread_move(struct pt *pt))
 {
     PT_BEGIN(pt);
-
+    
+    
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //ONLY NEED TO MESS WITH THESE VALUES
+    
+    
+    
     //modify the integer value on the right to get an appropriate travel distance
-    stp1.stpLeft = 400;
-    //stp2.stpLeft = 400;
-
+    //stp1.stpLeft = 800;
+    stp3.stpLeft = 800;
+    
+    
+    
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    
+    
+    //setting up for the ISR to move the position
+    keep_moving = 1;
+    //halting until the desired position is reached
+    PT_YIELD_UNTIL(&pt_move, keep_moving == 0);
            
             
     PT_END(pt);
@@ -130,11 +146,11 @@ int main(void)
     
     // === Config timer and output compares to make pulses ========
     // set up timer2 to generate the wave period
-    OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, generate_period);
+    OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, generate_period_xy);
     ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);
     mT2ClearIntFlag(); // and clear the interrupt flag
     
-    OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_1,120000);
+    OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_1,generate_period_z);
     ConfigIntTimer3(T3_INT_ON | T3_INT_PRIOR_2);
     mT3ClearIntFlag(); // and clear the interrupt flag
   
@@ -158,34 +174,37 @@ int main(void)
     
     
     //DECLARING BITS FOR STEPPER PINS (Stepper 3)
-//    mPORTBSetPinsDigitalOut(BIT_8);
-//    mPORTBSetPinsDigitalOut(BIT_9);
-//    mPORTBSetPinsDigitalOut(BIT_10);
-//    mPORTBSetPinsDigitalOut(BIT_11);
-//    mPORTBClearBits(BIT_8);
-//    mPORTBClearBits(BIT_9);
-//    mPORTBClearBits(BIT_10);
-//    mPORTBClearBits(BIT_11);
+    mPORTBSetPinsDigitalOut(BIT_8);
+    mPORTBSetPinsDigitalOut(BIT_9);
+    mPORTBSetPinsDigitalOut(BIT_10);
+    mPORTBSetPinsDigitalOut(BIT_11);
+    mPORTBClearBits(BIT_8);
+    mPORTBClearBits(BIT_9);
+    mPORTBClearBits(BIT_10);
+    mPORTBClearBits(BIT_11);
     
     //setting direction pins for the motors
     stp1.DIR = BIT_0;
-//    stp3.DIR = BIT_8;
+    stp3.DIR = BIT_8;
     
     //setting direction for the motors
     stp1.dirMove = 0;
-//    stp3.NDIR = BIT_9;
-//    stp3.NSTP = BIT_11;
+    stp3.NDIR = BIT_9;
+    stp3.NSTP = BIT_11;
 
     //setting step ppins for motors
     stp1.STP = BIT_1;
-//    stp3.STP = BIT_10;
+    stp3.STP = BIT_10;
 
     //setting the steps remaining for each motor
     stp1.stpLeft = 0;
-//    stp3.stpLeft = 0;
+    stp3.stpLeft = 0;
 
     // schedule the threads
-
+    int q=0;
+    while(q<10000000){
+        q++;
+    }
     PT_SCHEDULE(protothread_move(&pt_move));
 
 } // main
