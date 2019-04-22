e06fcc6bf5baf1536a1decba456fcef61019ac54
youngpines <hy379@cornell.edu>
2019-04-22 14:19:23 -0400

    updated scripts to test

---
+++ Scripts/image2process.py
@@ -0,0 +1,61 @@
+# Importing the basic Image library 
+from PIL import Image
+import numpy as np
+import serial, time
+
+# Opening up a serial port to transmit to the Microcontroller
+#ser = serial.Serial(
+#    port='/dev/ttyUSB0',        #the port can change based on which is used
+#    baudrate=9600,
+#    parity= serial.PARITY_NONE,
+#    stopbits=1,
+#    bytesize=8
+#)
+#print("Using [ " + ser.name + " ]")
+
+# Opening up the target image
+im = Image.open('/home/ho-jung/Desktop/Junior/SCULPT/TestImages/black_c.jpg')
+
+#displaying the target image for checking function
+#im.show()
+
+#converting the image to greyscale and resizing
+grey = im.convert('L')
+# size = 248, 128
+size = 5, 6
+grey = grey.resize(size,Image.LANCZOS)
+
+#displaying the final to ensure it worked well
+#grey.show()
+
+#format is [width][height]
+pixels = np.asarray(grey, dtype=np.uint8)
+
+#cycling through and writing all pixel values to the PIC
+count = 0
+print("[ %s ] dimension of array" % str(pixels.shape))
+
+pixFlat = pixels.ravel()
+pixSorted = np.dstack(np.unravel_index(np.argsort(pixFlat, kind="stable"),
+    pixels.shape))
+pixSorted = pixSorted.reshape(30, 2)
+pixVals = np.sort(pixFlat)
+imageData = np.column_stack((pixSorted[::-1], pixVals[::-1]))
+
+print("before")
+#print(pixels)
+print("size")
+print(imageData.shape)
+print("imagedata")
+print(imageData)
+
+#Reprinting to make copy/paste friendly
+happy = str(list(imageData)).replace("array(","").replace(")", "").replace('[',
+        '\n{').replace(']', '}')
+print(happy)
+
+exit = "e\r"
+#ser.write(str.encode(exit))
+time.sleep(1)
+#ser.close()
+print("[ Done ] All pixels Loaded")
